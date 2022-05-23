9964f1de4590f398e1364017632381dac74505e8
yerim <gyr3904@gmail.com>
2022-05-23 17:03:35 +0900

    multer공부중

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
-const path= require('path');
+const path = require('path');
 
-const app = express();
 dotenv.config();
-
-app.set('port', process.env.PORT || 3000); //포트번호->3000 정의
+const app = express();
+app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname,'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave:false,
-    saveUninitialized: false,
-    secret:process.env.COOKIE_SECRET,
-    cookie:{
-        httpOnly:true,
-        secure:false,
-    },
-    name:'session-cookie',
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'session-cookie',
 }));
-app.use((req,res,next)=>{
 
-console.log('모든 요청에 다 실행됩니다.');
-next();
+const multer = require('multer');
+const fs = require('fs');
+
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
+}
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads/');
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+app.get('/upload', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.post('/upload', upload.single('image'), (req, res) => {
+  console.log(req.file);
+  res.send('ok');
+});
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
 });
